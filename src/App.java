bdb10e17d731276ee1449be82be9a4528f2a65cc
gabrielyamihige <gabriel20010423@gmail.com>
2022-10-03 10:36:57 -0300

    salvando

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        System.out.println("Escreva um numero");
+        a = sc.nextInt();
+
+        System.out.println("O numero informado e " + a);
+    }
+}
